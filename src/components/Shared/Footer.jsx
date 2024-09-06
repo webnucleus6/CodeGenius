b034b0e6b93c0c4c5d884441050b9f7f4e55d861
webdev-mostofa <mdmostofaa24@gmail.com>
2024-09-06 15:54:16 +0600

    update:footer

--- src/components/Shared/Footer.jsx
+++ src/components/Shared/Footer.jsx
@@ -8,54 +8,79 @@ import {
 const Footer = () => {
   return (
     <footer className="bg-[#061A3A] mt-8 md:mt-20 lg:mt-48 pt-5">
-    <div className="px-4 sm:px-6 text-white sm:grid md:grid-cols-4 sm:grid-cols-2 mx-auto">
-      <div className="p-5">
-        <a className="btn btn-ghost  ">
+      <div className="px-4 sm:px-6 text-white flex flex-col items-center md:items-start md:flex-row justify-between">
+        <div className="p-5 text-center md:text-start">
+          <a href="#" className="">
             <div className="text-[0px]">
               {" "}
               <span className="text-xl">Code</span>
               <span className="text-2xl font-bold">Genius</span>
             </div>
           </a>
+        </div>
+        <div className="p-5 text-center md:text-start">
+          <div className="text-sm uppercase  font-bold">Services</div>
+          <a className="my-3 block" href="/#">
+            Branding <span className="text-teal-600 text-xs p-1"></span>
+          </a>
+          <a className="my-3 block" href="/#">
+            Design <span className="text-teal-600 text-xs p-1"></span>
+          </a>
+          <a className="my-3 block" href="/#">
+            Development
+          </a>
+        </div>
+        <div className="p-5 text-center md:text-start">
+          <div className="text-sm uppercase  font-bold">Company</div>
+          <a className="my-3 block" href="/#">
+            Help Center <span className="text-teal-600 text-xs p-1"></span>
+          </a>
+          <a className="my-3 block" href="/#">
+            Privacy Policy <span className="text-teal-600 text-xs p-1"></span>
+          </a>
+          <a className="my-3 block" href="/#">
+            Conditions <span className="text-teal-600 text-xs p-1"></span>
+          </a>
+        </div>
+        <div className="p-5 text-center md:text-start">
+          <div className="text-sm uppercase  font-bold">Contact us</div>
+          <ul className="flex items-center justify-center sm:justify-start mt-5 gap-5">
+            <li>
+              <a href="#">
+                <FaFacebook />
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <FaTwitter />
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                {" "}
+                <FaLinkedin />
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <FaInstagram />
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <FaYoutube />
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
-      <div className="p-5">
-        <div className="text-sm uppercase  font-bold">Services</div>
-        <a className="my-3 block" href="/#">Branding <span className="text-teal-600 text-xs p-1"></span></a>
-        <a className="my-3 block" href="/#">Design <span className="text-teal-600 text-xs p-1"></span></a>
-        <a className="my-3 block" href="/#">Development <span className="text-teal-600 text-xs p-1">New</span></a>
-      </div>
-      <div className="p-5">
-        <div className="text-sm uppercase  font-bold">Company</div>
-        <a className="my-3 block" href="/#">Help Center <span className="text-teal-600 text-xs p-1"></span></a>
-        <a className="my-3 block" href="/#">Privacy Policy <span className="text-teal-600 text-xs p-1"></span></a>
-        <a className="my-3 block" href="/#">Conditions <span className="text-teal-600 text-xs p-1"></span></a>
-      </div>
-      <div className="p-5">
-        <div className="text-sm uppercase  font-bold">Contact us</div>
-        <ul className="flex items-center mt-5 gap-5">
-          <li>
-            <FaFacebook />
-          </li>
-          <li>
-            <FaTwitter />
-          </li>
-          <li>
-            <FaLinkedin />
-          </li>
-          <li>
-            <FaInstagram />
-          </li>
-          <li>
-            <FaYoutube />
-          </li>
-        </ul>
-      </div>
-    </div>
 
-    <div className="bg-[#092349] py-4">
-    <p className="text-white text-center text-sm">© 2024 CodeGenius. All rights reserved.</p>
-    </div>
-  </footer>
+      <div className="bg-[#092349] py-4">
+        <p className="text-white text-center text-sm">
+          © 2024 CodeGenius. All rights reserved.
+        </p>
+      </div>
+    </footer>
   );
 };
 
